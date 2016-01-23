99d6703ae7f5427306828fd3edbd3bac3a9dd903
Raja sekar <m.rajasekar17@gmail.com>
2016-01-23 07:22:06 +0530

    PostgreConnection

--- src/main/java/SqlToMongo/SqltoMongo.java
+++ src/main/java/SqlToMongo/SqltoMongo.java
@@ -1,5 +1,50 @@
 package SqlToMongo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class SqltoMongo {
+	
+	public ArrayList<String> readPostgre(){
+		String usr ="postgres";
+		String pwd ="1234";
+		String url ="jdbc:postgresql://localhost:5432/mydb";
+		 ArrayList<String> values = new ArrayList<String>();
+		try
+		{
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Success loading Driver!");
+		}
+
+		catch(Exception e)
+		{
+			System.out.println("Fail loading Driver!");
+			e.printStackTrace();
+		}
+		try {
+            Connection dateabaseConnection = DriverManager.getConnection(url, usr, pwd);
+            System.out.println("Success connecting server!");
+
+            Statement statementObject = dateabaseConnection.createStatement();
+            ResultSet rs = statementObject.executeQuery("SELECT * FROM Sales");
+           
+            while(rs.next()){
+            	values.add(rs.getString("cust"));
+            	values.add(rs.getString("prod"));
+            	values.add(rs.getString("day"));
+            	values.add(rs.getString("month"));
+            	values.add(rs.getString("year"));
+            	values.add(rs.getString("quant"));
+            }
+           
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		 return values;
+	}
 
 }
--- src/main/webapp/Index.jsp
+++ src/main/webapp/Index.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@ page import="SqlToMongo.SqltoMongo" %>
+    <%@ page import="java.util.*" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +10,8 @@
 </head>
 <body>
 <h1> Welcome to Citizens Bank</h1>
+<%SqltoMongo sql = new SqltoMongo(); %>
+<% ArrayList<String> list = sql.readPostgre(); %>
+<%=list %>
 </body>
 </html>
