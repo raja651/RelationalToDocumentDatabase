db1a7602ec32517ea2616e884b1115b080cf5023
Raja sekar <m.rajasekar17@gmail.com>
2016-01-24 12:21:19 +0530

    mongo connector

--- src/main/java/SqlToMongo/SqltoMongo.java
+++ src/main/java/SqlToMongo/SqltoMongo.java
@@ -6,6 +6,11 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
 public class SqltoMongo {
 
 	public ArrayList<String> readPostgre(String db, String usr, String pwd) {
@@ -24,12 +29,7 @@ public class SqltoMongo {
 					.executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");
 
 			while (rs.next()) {
-				values.add(rs.getString("table_name"));
-				// values.add(rs.getString("prod"));
-				// values.add(rs.getString("day"));
-				// values.add(rs.getString("month"));
-				// values.add(rs.getString("year"));
-				// values.add(rs.getString("quant"));
+				values.add(rs.getString("table_name"));				
 			}
 			databaseConnection.close();
 		} catch (Exception e) {
@@ -44,54 +44,59 @@ public class SqltoMongo {
 
 		String url = "jdbc:postgresql://localhost:5432/"+db;
 		postgresDriver();
+		int columnCount = 0;
 		String[] columnNames;
-		ArrayList tableValues;
+		ArrayList<String> tableValues;
 		try {
 			Connection dataabaseConnection = null;
 			dataabaseConnection = DriverManager.getConnection(url, usr, pwd);
 			System.out.println("Success connecting server!");
 
 			Statement statementObject = dataabaseConnection.createStatement();
-			int rowcount = 0;
-			String s ="Select count(*) from "+ tableName;
-			ResultSet rs = statementObject.executeQuery(s);
-			System.out.println(rs);
-				
+			
+			String s ="Select count(*) FROM information_schema.columns WHERE table_schema='public'AND table_name="+"'"+tableName+"'";
+			ResultSet rs = statementObject.executeQuery(s);			
+			
+			while(rs.next()){				
+				columnCount=rs.getInt("count");
+			}
+			System.out.println(columnCount);
 			
 			String sqll = "SELECT * FROM information_schema.columns WHERE table_schema='public'AND table_name="+"'"+tableName+"'";
 			rs= statementObject.executeQuery(sqll);					
 			
-			columnNames = new String[rowcount];		
+			columnNames = new String[columnCount];		
 			int i=0;
 			while (rs.next()) {			
 				columnNames[i]=rs.getString("column_name");
 				i++;
-			}
-			for(String col :columnNames){
-				System.out.println(col);
-			}
+			}			
 
 			String sql = "SELECT * FROM "+ tableName;
-			rs = statementObject.executeQuery(sql);
-			System.out.println("1111111111111111111111111111111111111");
-			System.out.println(rs.getRow());
-			System.out.println(rs.getFetchSize());
-			System.out.println(rs.toString().length());
-			System.out.println("2222222222222222222222222222222222222222");
-			tableValues = new ArrayList();
+			rs = statementObject.executeQuery(sql);			
+																						
+			tableValues = new ArrayList<String>();
 			int k;
-//			while(rs.next()){
-//				for(k=0; k<rs1.getRow();k++)
-//					tableValues.add(rs.getString(k));				
-//			}
+			while(rs.next()){
+				for(k=1; k<columnCount;k++)
+					tableValues.add(rs.getString(k));				
+			}
+			for (String temp : tableValues) {
+				System.out.println(temp);
+			}
+
+			System.out.println(tableValues);
+			dataabaseConnection.close();	
 			
-			dataabaseConnection.close();
+			/* call mongo connector and push the values in MongoDB*/
+			mongoConnector(columnCount,columnNames,tableValues,tableName);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e);
 		}
 
+		
 		return null;
 
 	}
@@ -106,6 +111,38 @@ public class SqltoMongo {
 			System.out.println("Fail loading Driver!");
 			e.printStackTrace();
 		}
+	}
+	
+	public void mongoConnector(int columnCount, String[] columnNames, ArrayList<String> tableValues, String tableName){   
+		try{   
+			
+	         // To connect to mongodb server
+	         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+				
+	         // Now connect to your databases
+	         DB db = mongoClient.getDB( "test" );
+	         System.out.println("Connect to database successfully");
+				
+//	         boolean auth = db.authenticate(myUserName, myPassword);
+//	         System.out.println("Authentication: "+auth);
+				
+	         DBCollection coll = db.createCollection(tableName, null);
+	         System.out.println("Collection created successfully");
+	         
+	         BasicDBObject inQuery = new BasicDBObject();
+		    	
+		    	System.out.println(inQuery);		    	
+		    	coll.insert(inQuery);
+	         
+				
+//	         DBCollection collc = db.getCollection("mycol");
+//	         System.out.println("Collection mycol selected successfully");
+	         
+	         
+	      }catch(Exception e){
+	         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      }
+		
 	}
 
 }
